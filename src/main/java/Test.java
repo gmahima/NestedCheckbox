7ba02dfc79b7f2729f282086ee20d78418c8cf72
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-07-09 22:47:01 +0530

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,32 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Test extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Button b = new Button("hi");
+        Scene scene = new Scene(b);
+
+        primaryStage.setTitle("JotArthur | Image");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+}
